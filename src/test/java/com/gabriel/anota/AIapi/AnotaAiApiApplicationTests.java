24e8552efdc46f2890090c712e64487394c38296
Jason_Gabriel <j.gabrielaraujodasilva@gmail.com>
2024-05-02 09:17:25 -0300

    feat: Initial commit

---
+++ src/test/java/com/gabriel/anota/AIapi/AnotaAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.anota.AIapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
